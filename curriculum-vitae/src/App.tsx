92c2f50b049eef8542e06e47559a9145e68009ce
stefan.alistar <stefan.alistar@codemart.ro>
2024-05-07 20:33:04 +0300

    CV-1: Added theme

--- curriculum-vitae/src/App.tsx
+++ curriculum-vitae/src/App.tsx
@@ -1,12 +1,18 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import './App.css'
 import Header from './components/header/Header.tsx'
+import { appTheme } from './themes/theme.ts'
 
 function App() {
 
   return (
-    <>
-      <Header/>
-    </>
+    <ThemeProvider theme={appTheme}>
+      <CssBaseline enableColorScheme />
+      <>
+        <Header/>
+      </>
+    </ThemeProvider>
+    
   )
 }
 
