eed309180614b2d8c72916c111cc4642dd28fa74
stefan.alistar <stefan.alistar@codemart.ro>
2024-05-18 22:50:44 +0300

    CV-1: Added MUI treasury

---
+++ curriculum-vitae/src/mui-treasury/card-project/CardProject.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Info, InfoEyebrow, InfoSubtitle, InfoTitle } from "../info-basic";
+import { getInfoN04Styles } from "../info-n04";
+
+export function CardProject() {
+  return (
+    <Box sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}>
+      <Card
+        sx={{
+          maxWidth: 600,
+          borderRadius: "20px",
+          boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+          transition: "0.3s",
+          alignItems: "center",
+          marginTop: 10, 
+        }}
+      >
+        <Box sx={{ minWidth: 256 }}>
+          <Box
+            sx={{
+              padding: `4px 24px 0`,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Avatar
+              alt={"brand logo"}
+              sx={(theme) => ({
+                width: 48,
+                height: 48,
+                transform: "translateY(50%)",
+                "& > img": {
+                  margin: 0,
+                  backgroundColor: "common.white",
+                },
+                [theme.breakpoints.up("sm")]: {
+                  width: 60,
+                  height: 60,
+                },
+              })}
+            />
+            <Typography
+              sx={{
+                textTransform: "uppercase",
+                fontSize: 14,
+                color: "grey.500",
+                letterSpacing: "1px",
+              }}
+            >
+              Experience
+            </Typography>
+          </Box>
+          <Box
+            component="hr"
+            sx={(theme) => ({
+              backgroundColor: "grey.200",
+              marginBottom: `${24 - 1}px`, // minus 1 due to divider height
+              [theme.breakpoints.up("sm")]: {
+                marginBottom: `${30 - 1}px`, // minus 1 due to divider height
+              },
+            })}
+          />
+        </Box>
+        <CardContent sx={{ p: 3 }}>
+          <Info useStyles={getInfoN04Styles}>
+            <InfoEyebrow>Codemart</InfoEyebrow>
+            <InfoTitle>Full-stack developer</InfoTitle>
+            <InfoSubtitle>
+              Descriere despre ce am facut la job, tehnologii folosite, proiecte in mare etc.
+            </InfoSubtitle>
+          </Info>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+}
