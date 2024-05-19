1fb4fa326ea5fc15142488300690cd702b20c4aa
stefan.alistar <stefan.alistar@codemart.ro>
2024-05-19 22:15:35 +0300

    CV-1: Changed Contact

--- curriculum-vitae/src/components/contact/Contact.tsx
+++ curriculum-vitae/src/components/contact/Contact.tsx
@@ -1,44 +1,95 @@
-import { Box, Card, CardContent, Grid, TextField, Typography } from "@mui/material"
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 function Contact() {
-    return(
-
-        <>
-        <Box sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 10
-                }}>
-            <Card sx={{width: 500}}>
-                <CardContent>
-                    <Typography variant='h5' sx={{paddingBottom: 1}}>
-                        Contact
-                    </Typography>
-                <form>
-                    <Grid container spacing={1}>
-                        <Grid xs={12} sm={6} item>
-                            <TextField label='First name' placeholder='Enter first name' variant='outlined' fullWidth required></TextField>
-                        </Grid>
-                        <Grid xs={12} sm={6} item>
-                            <TextField label='Last name' placeholder='Enter last name' variant='outlined' fullWidth required></TextField>
-                        </Grid>
-                        <Grid xs={12} item>
-                            <TextField label='Email' placeholder='Enter email name' variant='outlined' fullWidth required></TextField>
-                        </Grid>
-                        <Grid xs={12} item>
-                            <TextField label='Phone number' placeholder='Enter phone number name' variant='outlined' fullWidth required></TextField>
-                        </Grid>
-                        <Grid xs={12} item>
-                            <TextField label='Message' placeholder='Leave me a message' variant='outlined' fullWidth required></TextField>
-                        </Grid>
-                    </Grid>
-                </form>
-                </CardContent>
-            </Card>
-        </Box>
-        </>
-    )
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 10,
+        }}
+      >
+        <Card
+          sx={{
+            maxWidth: 600,
+            borderRadius: "20px",
+            boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+            transition: "0.3s"
+          }}
+        >
+          <CardContent>
+            <Typography variant="h5" sx={{ paddingBottom: 1 }}>
+              Contact
+            </Typography>
+            <form>
+              <Grid container spacing={1}>
+                <Grid xs={12} sm={6} item>
+                  <TextField
+                    label="First name"
+                    placeholder="Enter first name"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  ></TextField>
+                </Grid>
+                <Grid xs={12} sm={6} item>
+                  <TextField
+                    label="Last name"
+                    placeholder="Enter last name"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  ></TextField>
+                </Grid>
+                <Grid xs={12} item>
+                  <TextField
+                    label="Email"
+                    placeholder="Enter email name"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  ></TextField>
+                </Grid>
+                <Grid xs={12} item>
+                  <TextField
+                    label="Phone number"
+                    placeholder="Enter phone number name"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  ></TextField>
+                </Grid>
+                <Grid xs={12} item>
+                  <TextField
+                    label="Message"
+                    placeholder="Leave me a message"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    multiline
+                    rows={5}
+                  ></TextField>
+                </Grid>
+                <Button type="submit" color="primary" fullWidth>
+                  Submit
+                </Button>
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+      </Box>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
